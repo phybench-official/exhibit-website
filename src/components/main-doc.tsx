66d2574c18f24d1d8f3cb1041bb956c4f811fa67
wjsoj <wjs@wjsphy.top>
2025-05-09 21:06:06 +0800

    revert: :rewind: revert to original

---
+++ src/components/main-doc.tsx
@@ -0,0 +1,8 @@
+export function MainDoc() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold mb-4">Documentation</h1>
+      <p className="text-lg">This is the documentation page.</p>
+    </div>
+  )
+}
